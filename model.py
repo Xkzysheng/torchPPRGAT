795cc9fa735ae488f03a63e1576ccb0a7186b458
Xkzysheng <52355507+Xkzysheng@users.noreply.github.com>
2024-10-18 13:46:18 +0800

    Create model.py

---
+++ model.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from PPRGATConv import PersonalizedPageRankGraphAttentionLayer as PPRGATLayer
+
+class PPRGAT(nn.Module):
+    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads, n_nodes):
+        """Dense version of PPRGAT (Single head, Without multi-heads)."""
+        super(HAT, self).__init__()
+        # hidden_layer
+        self.attentions = PPRGATLayer(nfeat, nhid, n_nodes=n_nodes, concat=False)
+        # out_layer
+        self.out_att = PPRGATLayer(nhid, nclass, n_nodes=n_nodes, concat=False)
+
+    def forward(self, x, adj):
+        x = self.attentions(x, adj)
+        x = F.elu(x)
+        x = self.out_att(x, adj)
+        # 分类问题，最后一层过一遍softmax
+        return x # 配合crossentropy_loss (隐含softmax实现）
